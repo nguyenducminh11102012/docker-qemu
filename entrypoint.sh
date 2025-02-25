e0ce9bc563251761ee559d8001f4ff24860dfd15
nguyenducminh11102012 <nguyenducminh11102012@gmail.com>
2025-02-25 18:25:11 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,10 @@
 set -e
 
 # Function for extracting keys from etcdctl if they exist
-# sets getKeyReturn to value of requested key (or "" if it doesn't exist)
 function getKey() {
    getKeyReturn=""
-   if [ -n $1 ]; then
-      val=$(etcdctl get $1)
+   if [ -n "$1" ]; then
+      val=$(etcdctl get "$1")
       if [ $? -eq 0 ]; then
          getKeyReturn=$val
       fi
@@ -14,29 +13,9 @@ function getKey() {
    return
 }
 
-# Create the kvm node (required --privileged)
-if [ ! -e /dev/kvm ]; then
-   set +e
-   mknod /dev/kvm c 10 $(grep '\<kvm\>' /proc/misc | cut -f 1 -d' ')   
-   set -e
-fi
-
-# If we have a BRIDGE_IF set, add it to /etc/qemu/bridge.conf
-if [ -n "$BRIDGE_IF" ]; then
-   echo "allow $BRIDGE_IF" >/etc/qemu/bridge.conf
-
-   # Make sure we have the tun device node
-   if [ ! -e /dev/net/tun ]; then
-      set +e
-      mkdir -p /dev/net
-      mknod /dev/net/tun c 10 $(grep '\<tun\>' /proc/misc | cut -f 1 -d' ')
-      set -e
-   fi
-fi
-
 # If we were given arguments, override the default configuration
 if [ $# -gt 0 ]; then
-   exec /usr/bin/kvm $@
+   exec qemu-system-x86_64 "$@"
    exit $?  # Make sure we really exit
 fi
 
@@ -48,13 +27,13 @@ SPICE_PORT=${SPICE_PORT:-$(getKey /kvm/${INSTANCE}/spice_port; echo $getKeyRetur
 EXTRA_FLAGS=${EXTRA_FLAGS:-$(getKey /kvm/${INSTANCE}/extra_flags; echo $getKeyReturn)}
 
 # If we do not have the host lock, abort
-if [ ${HOSTNAME} != $(etcdctl get /kvm/${INSTANCE}/host) ]; then
+if [ "${HOSTNAME}" != "$(etcdctl get /kvm/${INSTANCE}/host)" ]; then
    echo "We do not have a host lock; aborting"
    exit 1
 fi
 
 # Execute with default settings
-exec /usr/bin/kvm -vga qxl -spice port=${SPICE_PORT},addr=127.0.0.1,disable-ticketing \
+exec qemu-system-x86_64 -vga qxl -spice port=${SPICE_PORT},addr=127.0.0.1,disable-ticketing \
    -k en-us -m ${VM_RAM} -cpu qemu64 \
    -netdev bridge,br=${BRIDGE_IF},id=net0 -device virtio-net,netdev=net0,mac=${VM_MAC} \
    -drive format=rbd,file=rbd:${VM_RBD},cache=writeback,if=virtio ${EXTRA_FLAGS}
